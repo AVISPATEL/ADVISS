b9e854e9233fa785f3c3786ad994a3dffc1f895f
AVISPATEL <37960105+AVISPATEL@users.noreply.github.com>
2020-07-28 08:20:16 +0530

    Faculty details class is created

---
+++ faculty_details/src/main/java/com/avi/faculty_details/FacultyDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.avi.faculty_details;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacultyDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacultyDetailsApplication.class, args);
+	}
+
+}
