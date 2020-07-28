b283e94fb3c24d653244e4557020c1653cf90a41
AVISPATEL <37960105+AVISPATEL@users.noreply.github.com>
2020-07-28 08:19:05 +0530

    Clek details class is created

---
+++ clerk_details/src/main/java/com/avi/clerk_details/ClerkDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.avi.clerk_details;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClerkDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClerkDetailsApplication.class, args);
+	}
+
+}
