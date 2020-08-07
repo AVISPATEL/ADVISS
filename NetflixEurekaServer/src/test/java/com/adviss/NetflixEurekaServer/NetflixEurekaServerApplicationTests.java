c755cbcd0b7e18bbf6ca94d8a928006863ba07a6
AVISPATEL <37960105+AVISPATEL@users.noreply.github.com>
2020-08-07 18:26:40 +0530

    Create Netflix Eureka Server

---
+++ NetflixEurekaServer/src/test/java/com/adviss/NetflixEurekaServer/NetflixEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adviss.NetflixEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
